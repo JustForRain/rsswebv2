50b80e4d1840fd89b1e37a5198b7e113923e3083
303138798@qq.com <guawaziXY1234>
2018-03-03 22:06:31 +0800

    开启Spring MVC Json注解

--- src/main/java/com/pcrain/rsswebv2/controller/DownController.java
+++ src/main/java/com/pcrain/rsswebv2/controller/DownController.java
@@ -3,9 +3,12 @@ package com.pcrain.rsswebv2.controller;
 import com.pcrain.rsswebv2.domain.vo.LayUIDataGridResult;
 import com.pcrain.rsswebv2.service.DownService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @RequestMapping("down")
 public class DownController {
     @Autowired
@@ -17,6 +20,7 @@ public class DownController {
      * @return
      */
     @RequestMapping("recent")
+    @ResponseBody
     public LayUIDataGridResult getRecentDown(@RequestParam(defaultValue = "1")Integer page){
         LayUIDataGridResult result=downService.getRecentDown(page);
         return result;
