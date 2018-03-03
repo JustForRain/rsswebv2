0cb42a862fc9026cfeeb186d6bb9e2b8bcfee2c4
303138798@qq.com <guawaziXY1234>
2018-03-03 17:27:59 +0800

    修改springmvc xml只扫描controller，spring容器扫描除controller以外的其他注解，添加LayUI前端Bean，web.xml添加spring容器

---
+++ src/main/java/com/pcrain/rsswebv2/controller/DownController.java
@@ -0,0 +1,14 @@
+package com.pcrain.rsswebv2.controller;
+
+import com.pcrain.rsswebv2.domain.vo.LayUIDataGridResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("down")
+public class DownController {
+//    @RequestMapping("recent")
+//    public LayUIDataGridResult getRecentDown(@RequestParam(defaultValue = "1")Integer page,@RequestParam(defaultValue = "30")Integer rows){
+//        LayUIDataGridResult result=;
+//        return result;
+//    }
+}
