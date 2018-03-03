df8ece6ecf242cd879b155bbaadaa0f39e50ea1c
303138798@qq.com <guawaziXY1234>
2018-03-03 15:03:52 +0800

    完成/index.do 跳转及基本SpringMVC配置

---
+++ src/main/java/com/pcrain/rsswebv2/controller/PageController.java
@@ -0,0 +1,12 @@
+package com.pcrain.rsswebv2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("index")
+    public String IndexPage(){
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>灰大郎的RSS订阅站</title>
+</head>
+<body>
+灰大郎的RSS订阅站
+</body>
+</html>
